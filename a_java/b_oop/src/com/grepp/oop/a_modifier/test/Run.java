63a216588a37a7b983966a9d553690508db41587
leeseo0912 <ef032550@naver.com>
2025-03-07 17:20:50 +0900

    03/07

---
+++ a_java/b_oop/src/com/grepp/oop/a_modifier/test/Run.java
@@ -0,0 +1,11 @@
+package com.grepp.oop.a_modifier.test;
+
+import com.grepp.oop.a_modifier.A_modifier;
+
+public class Run {
+
+  public static void main(String[] args) {
+    A_modifier.publicMethod();
+  }
+
+}
---
+++ a_java/b_oop/src/com/grepp/oop/b_init/Run.java
@@ -0,0 +1,9 @@
+package com.grepp.oop.b_init;
+
+public class Run {
+
+  public static void main(String[] args) {
+    A_init init = new A_init("이서");
+    //A_init.loadClass();
+  }
+}
