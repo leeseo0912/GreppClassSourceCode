4051b18a233756512cd0375bbceb93b5b53c3f47
leeseo0912 <ef032550@naver.com>
2025-03-11 12:44:53 +0900

    commit 03/10

---
+++ d_library/src/com/grepp/library/a_wrapper/A_wrapper.java
@@ -0,0 +1,28 @@
+// 2025.03.11 (화)
+package com.grepp.library.a_wrapper;
+
+public class A_wrapper {
+
+    private boolean activated;
+    private Boolean wActivated;
+    private int num;
+    private Integer wNum;
+    private char ch;
+    private Character wCh;
+    private double dNum;
+    private Double wDNum;
+
+    @Override
+    public String toString() {
+        return "A_wrapper{" +
+                "activated=" + activated +
+                ", wActivated=" + wActivated +
+                ", num=" + num +
+                ", wNum=" + wNum +
+                ", ch=" + ch +
+                ", wCh=" + wCh +
+                ", dNum=" + dNum +
+                ", wDNum=" + wDNum +
+                '}';
+    }
+}
