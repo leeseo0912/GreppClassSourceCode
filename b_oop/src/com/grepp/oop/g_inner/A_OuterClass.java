e399b44f7060d4e8258a05e175db69b056530cfd
leeseo0912 <ef032550@naver.com>
2025-03-12 10:02:27 +0900

    commit 03/12

--- b_oop/src/com/grepp/oop/g_inner/A_OuterClass.java
+++ b_oop/src/com/grepp/oop/g_inner/A_OuterClass.java
@@ -1,6 +1,28 @@
 // 2025.03.12 (수)
 package com.grepp.oop.g_inner;
 
-public class A_Outer {
+public class A_OuterClass {
+    private String desc;
 
+  public A_OuterClass(String desc) {
+    this.desc = desc;
+  }
+
+  // NOTE A01 Static Inner Class
+  public static class StaticInner{
+    public void print() {
+      System.out.println("Static Inner Class 입니다.");
+      // 객체의 필드에는 접근 못함
+    }
+  }
+
+  // NOTE A02 Instanc Inner Class
+  public class InstanceInner{
+    public void print(){
+      System.out.println("=================================");
+      System.out.println(A_OuterClass.this);
+      System.out.println(this);
+      System.out.println(desc);
+    }
+  }
 }
--- b_oop/src/com/grepp/oop/g_inner/Run.java
+++ b_oop/src/com/grepp/oop/g_inner/Run.java
@@ -1,4 +1,19 @@
+// 2025.03.12 (수)
 package com.grepp.oop.g_inner;
 
+import com.grepp.oop.g_inner.A_OuterClass.InstanceInner;
+import com.grepp.oop.g_inner.A_OuterClass.StaticInner;
+
 public class Run {
+
+  public static void main(String[] args) {
+
+    StaticInner staticInner = new A_OuterClass.StaticInner();
+    staticInner.print();
+
+    A_OuterClass a = new A_OuterClass("outer");
+    InstanceInner instanceInner = a.new InstanceInner();
+    instanceInner.print();
+
+  }
 }
