1bfa2558c996c4394e194605c09be9e2674ac181
leeseo0912 <ef032550@naver.com>
2025-03-12 10:50:47 +0900

    commit 03/12

---
+++ d_library/src/com/grepp/library/c_collection/a_generic/b_list/Run.java
@@ -0,0 +1,18 @@
+package com.grepp.library.c_collection.a_generic.b_list;
+
+public class Run {
+
+  public static void main(String[] args) {
+
+    // NOTE B01 : CRUD
+    // 데이터를 조작하는 방법을 줄여서 말함
+    // Create, Read, Update, Delete
+    pracAdd();
+  }
+
+  private static void pracAdd() {
+
+
+  }
+
+}
